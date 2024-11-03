93d53b3b88dac6bf9e245fa37fb2623951f7c01b
David Pereira <inexhero@gmail.com>
2024-11-03 15:53:17 -0600

    Base code

---
+++ index.js
@@ -0,0 +1,44 @@
+const http = require('bare-http1')
+const HolesailServer = require('./lib/bare-holesail-server')
+const router = require('./lib/bare-find-my-way')
+
+
+
+class Kiwi {
+  constructor(port = 3000){
+    this.server = this.server = http.createServer((req,res)=>{
+      this.handler(req,res)
+    })
+    this.port = port
+    this.router = router()
+
+  }
+
+  // TODO: Missing code
+  useMiddleware(base,...fns){
+    return this //apply chain
+  }
+
+  start(buffSeed = null){
+
+    this.server.listen(this.port,()=>{
+      console.log('Server Running')
+      console.log('127.0.0.1:'+this.port)
+      if (buffSeed !== null) {
+        const holeServer =  new HolesailServer();
+        holeServer.serve({port:this.port, address:"127.0.0.1",buffSeed:buffSeed}, () => {
+          console.log('Server started');
+          console.log(holeServer.getPublicKey());
+
+        })
+      }
+    })
+
+
+  }
+  handler=(req,res)=>{
+    this.router.lookup(req,res)
+  }
+}
+
+module.exports = Kiwi
